3a1728871ae0e71aae47f09e07a7133087c90df8
임유나 <libbyyouna@gmail.com>
2025-04-10 21:01:12 +0900

    words

--- Django_project/index/urls.py
+++ Django_project/index/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name='index'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('words/', views.words, name='words'),
 
 ]
--- Django_project/index/views.py
+++ Django_project/index/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'intro/index.html')
+
+def words(request):
+    return render(request, 'intro/words.html')
